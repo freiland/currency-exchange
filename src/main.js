8bd093751fc044c9e9e15f386e716dc06536600b
Forrest Reiland <you@example.com>
2020-09-11 15:30:18 -0700

    try to add functionality for two error messages

--- src/main.js
+++ src/main.js
@@ -17,25 +17,19 @@ $(document).ready(function() {
     
     let promise = currencyEx.getExRate();
     promise.then(function(response) {
+      
       const body = JSON.parse(response);
-      console.log(countryCode);
-      console.log(body);
+      
       let exRate = body.conversion_rates[countryCode];
       
-      console.log(exRate);
-      
-      let exNum = exRate;
-
-      console.log(exNum);
-      
-      console.log(body);
       let exchangeRound = Math.round((dollarAmount*exRate*100)) / 100;
+      
       $("#response2").text(`The value of $${dollarAmount} in ${countryCode} is:`);
       $('#response').text(exchangeRound); 
       
     }, function(error) {
-      $('#error-response').text('Dang it :/ There was an error </3');
-      console.log(error);
+      $('#error-response').text(`There was an error processing the request: ${error}`);
+      
     });
   }); 
 });
