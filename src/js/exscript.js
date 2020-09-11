d54a6b8e164c9bfa74a9508e45573093849c855a
Forrest Reiland <you@example.com>
2020-09-11 10:01:44 -0700

    update url and test api key call

--- src/js/exscript.js
+++ src/js/exscript.js
@@ -2,7 +2,7 @@ export default class currencyEx {
   static getExRate() {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      const url = "https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD";
       request.onload = function() {
         if(this.status === 200) {
           resolve(request.response);
